b3d3900090296b10ab4c70330861edc1f87a7915
kleinbw <kleinbw@vdi-LCB-IC-004.ad.uc.edu>
2024-04-03 14:04:22 -0400

    Added documentation to the files

--- Team2_Assignment09/functionsPackage/functions.py
+++ Team2_Assignment09/functionsPackage/functions.py
@@ -1,3 +1,16 @@
+# Name: TJ Harrington, Ben Klein, Ryan Wilkins
+# email: kleinbw@mail.uc.edu
+# Assignment Number: Assignment 09
+# Due Date: 04/04/24
+# Course/Section: IS 4010-002
+# Semester/Year: Spring 2024
+# Brief Description of the assignment: This is a group assignment and it has us call an API and display the API information.
+# Brief Description of what this module does: This file define the search_state_by_name function which will return the state.
+
+
+
+
+#Here we define our function to search for the state name
 def search_state_by_name(states_data, state_name):
     for state in states_data:
         if state['name'] == state_name:
--- Team2_Assignment09/mainPackage/main.py
+++ Team2_Assignment09/mainPackage/main.py
@@ -1,3 +1,17 @@
+# Name: TJ Harrington, Ben Klein, Ryan Wilkins
+# email: kleinbw@mail.uc.edu
+# Assignment Number: Assignment 09
+# Due Date: 04/04/24
+# Course/Section: IS 4010-002
+# Semester/Year: Spring 2024
+# Brief Description of the assignment: This is a group assignment and it has us call an API and display the API information.
+# Brief Description of what this module does: This module calls the API and uses the search_state_by_name function to create a new dictionary which we then print
+
+
+
+
+
+
 import json
 import requests
 
@@ -5,7 +19,7 @@ from functionsPackage.functions import*
 
 if __name__ == "__main__":
     url = "https://us-states.p.rapidapi.com/basic"
-    
+    #Here is the API Key
     headers = {
         "X-RapidAPI-Key": "e354a39aecmsh2c967112f19af0dp10dd88jsn9435c59da7e1",
         "X-RapidAPI-Host": "us-states.p.rapidapi.com"
@@ -16,7 +30,9 @@ if __name__ == "__main__":
     if response.status_code == 200:
         states_data = response.json()
         state_name = input("Enter the name of the state you want to search for: ")
+        #State Info is the dictionary for which the data is being stored
         state_info = search_state_by_name(states_data, state_name)
+        #Here we will be printing the information in state_info
         if state_info:
             print("State found:")
             print("Name:", state_info["name"])
